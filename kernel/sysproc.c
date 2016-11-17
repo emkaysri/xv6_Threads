f880c011be2d6ccc4b9e64838cc843e868de86e6
ssali3 <ssali3@wisc.edu>
2016-11-16 22:43:32 -0600

    Update sysproc.c
    Just putting up my notes from the videos and for future tasks in case they're at all helpful to reference

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -90,17 +90,28 @@ sys_uptime(void)
   return xticks;
 }
 
-int
-sys_clone(void)
-{
-
+  //Actual call is clone(void(*fcn)(void*), void *arg, void *stack) so those are args
+  // Basically copy from allocproc and fork but with adjustments so addr space/pgdir is copied from orig proc and stack is as per arg?
+  //fcn is first instr to exe, ret is pidof thread
+  //Adjust stuff about trap frame / registers
+  //Adjust wait to account for threads vs children (WAIT FREES AS OF CHILD SO MAKE SURE IT WON'T FREE THE SHARED AS OF THREADS) - probably just add a bool int to proc that's isthread or something and don't free if isthread
+  //Look at exec.c for stack allocation stuff (before clone is called, malloc a page to pass in as stack)
+     //Regs to set up include eip, esp
+  //See 2nd video for additional info
   return -1;
 }
 
 int
 sys_join(void)
 {
+  //Will presumably mostly copy from wait()?
   return -1;
 }
 
+//TODO:
+  //Also adjust exit() minimally for these changes
+  //Create thread lib
+  //Add to lib: thread_create(void(*start_routine)(void*), void*arg) which will basically be a wrapper for clone which mallocs a stack, then passes it and the args into clone()
+  //Add to lib: thread_join(), which is of course a wrapper for join()
+  //Add to lib: Create TYPE lock_t, and routines lock_acquire and lock_release (use x86 atomic xchg to make it work) and lock_init
 
