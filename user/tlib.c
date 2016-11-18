fb1f5986db915df4a9af9565d22c1c640c7f4514
ssali3 <ssali3@wisc.edu>
2016-11-18 15:27:13 -0600

    Update tlib.c

--- user/tlib.c
+++ user/tlib.c
@@ -1,41 +1,69 @@
-// TODO Add any include statements needed, add a header file for this so we can use the library in other files (might need to move parts of this to there), add stuff for this and its header to user makefile
+// TODO Add any additional include statements needed, create the global lock that the threads can access (held in proc state?)
 
-typedef struct__lock_t {
-  intflag;
-}lock_t;
+#include "types.h"
+#include "stat.h"
+#include "fcntl.h"
+#include "user.h"
+#include "x86.h"
 
-voidinit(lock_t*lock) {
-lock->flag = 0;
+
+#define PGSIZE		4096
+
+struct lock_t{
+  int flag;
+};
+
+void init(struct lock_t *lock) {
+  lock->flag = 0;
 }
 
-void acquire(lock_t*lock) {
-while(xchg(&lock->flag, 1) == 1) ;//spin-wait (do nothing)
+void acquire(struct lock_t *lock) {
+  while(xchg((uint*)(&lock->flag), 1) == 1) ;//spin-wait (do nothing)
 }
 
-void release(lock_t*lock) {
-lock->flag = 0;
+void release(struct lock_t*lock) {
+  lock->flag = 0;
 }
 
-int thread_create(void (*start_routine)(void*), void *arg){
-  acquire(lock_t*lock); 
-  // TODO Malloc stack and pass in as third arg
-  if(clone(start_routine, arg, vars) == 0){
-    release(lock_t*lock);
+int thread_create(void *start_routine, void *arg){
+  struct lock_t lock;
+  init(&lock); // TODO DELETE THESE LINES ONCE ACTUAL GLOBAL LOCK IS MADE
+  acquire(&lock);  // TODO Once the lock that will be used is defined pass it in as arg
+
+   /*void *stack = malloc(PGSIZE*2);
+   assert(stack != NULL);
+   if((uint)stack % PGSIZE)
+     stack = stack + (4096 - (uint)stack % PGSIZE);*/
+
+   void *stack = malloc(PGSIZE);
+   if (stack == NULL) return -1;
+
+
+  if(clone(start_routine, arg, stack) == 0){
+    release(&lock);
     return 0;
   } else  {
-    release(lock_t*lock);
+    release(&lock);
     return -1;
   }
+  /*release(&lock);
+  return -1;*/
 }
 
 int thread_join(){
-  acquire(lock_t*lock);
-  if(join(userstack) == 0){
-    free(userstack);
-    release(lock_t*lock);
+  struct lock_t lock;
+  init(&lock); // TODO DELETE THESE LINES ONCE ACTUAL GLOBAL LOCK IS MADE
+  acquire(&lock);
+  void *stack = malloc(PGSIZE);
+  if (stack == NULL) return -1;
+  if(join(&stack) == 0){
+    free(stack);
+    release(&lock);
     return 0;
   } else  {
-    release(lock_t*lock);
+    release(&lock);
     return -1;
   }
+  /*release(&lock);
+  return -1;*/
 }
