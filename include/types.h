0c43100006d0211193c07b9c6d254b6dc528cce2
ssali3 <ssali3@wisc.edu>
2016-11-23 16:39:18 -0600

    Update types.h

--- include/types.h
+++ include/types.h
@@ -7,6 +7,9 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+struct lock_t{
+  int flag;
+};
 #ifndef NULL
 #define NULL (0)
 #endif
